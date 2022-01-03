d12324529dc5945d8de346e39a4e647d88ae56bb
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-03 14:54:24 +0100

    creates content for Card component and types fn

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,19 @@ interface IState {
 }
 
 function App() {
-  const [people, setPeople] = useState<IState['people']>([])
+  const [people, setPeople] = useState<IState['people']>([
+    {
+      name: 'LeBron James',
+      age: 35,
+      url: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+      note: 'Allegeric to staying on the same team',
+    },
+    {
+      name: 'Kobe Bryant',
+      age: 42,
+      url: 'https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg',
+    },
+  ])
 
   return (
     <div className='App'>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -7,6 +7,23 @@ interface IProps {
   }[]
 }
 
-const List: React.FC<IProps> = ({ people }) => <div>i AM a list</div>
+const List: React.FC<IProps> = ({ people }) => {
+  const renderList = (): JSX.Element[] => {
+    return people.map(({ url, name, age, note }) => {
+      return (
+        <li className='List'>
+          <div className='List-header'>
+            <img className='List-img' src={url} />
+            <h2>{name}</h2>
+          </div>
+          <p>{age} years old</p>
+          <p className='List-note'>{note}</p>
+        </li>
+      )
+    })
+  }
+
+  return <ul>{renderList()}</ul>
+}
 
 export default List
